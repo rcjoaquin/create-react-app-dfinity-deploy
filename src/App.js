8f504bd765e45dac796134c061edf50f1a037459
sagostinelli <batonenbois@gmail.com>
2021-03-02 17:26:10 -0500

    my-first-dfinity-app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import logo from './logo.svg';
+import dfinityLogo from './dfinity-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={dfinityLogo} className="dfinity-logo" alt="logo" />
+        +
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This app is deployed on Dfinity through Fleek!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dfinity.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about Dfinity
+        </a>
+        <a
+          className="App-link"
+          href="https://fleek.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about Fleek
         </a>
       </header>
     </div>
